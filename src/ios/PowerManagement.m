4925eb4a91032d7c57789c2a71979c3c3ac1bb66
System Administrator <root@Enosiss-MacBook-Pro.local>
2014-12-01 13:11:15 +0600

    Removed all depricated methods and replaced them with new mthods

--- src/ios/PowerManagement.m
+++ src/ios/PowerManagement.m
@@ -29,8 +29,6 @@
 - (void) acquire:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* result = nil;
-    NSString* jsString = nil;
-    NSString* callbackId = command.callbackId;
     
     // Acquire a reference to the local UIApplication singleton
     UIApplication* app = [UIApplication sharedApplication];
@@ -39,22 +37,18 @@
         [app setIdleTimerDisabled:true];
         
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        jsString = [result toSuccessCallbackString:callbackId];
     }
     else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION messageAsString:@"IdleTimer already disabled"];
-        jsString = [result toErrorCallbackString:callbackId];
     }
     
-    [self writeJavascript:jsString];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 
 - (void) release:(CDVInvokedUrlCommand*)command
-{    
+{
     CDVPluginResult* result = nil;
-    NSString* jsString = nil;
-    NSString* callbackId = command.callbackId;
     
     // Acquire a reference to the local UIApplication singleton
     UIApplication* app = [UIApplication sharedApplication];
@@ -63,13 +57,11 @@
         [app setIdleTimerDisabled:false];
         
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        jsString = [result toSuccessCallbackString:callbackId];
     }
     else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION messageAsString:@"IdleTimer not disabled"];
-        jsString = [result toErrorCallbackString:callbackId];
     }
     
-    [self writeJavascript:jsString];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 @end
